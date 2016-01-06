489d15f9bf6912519f3cf4bd43e03989f51441bf
Rubiksron <rubiksron@gmail.com>
2016-01-06 13:21:27 -0800

    updated versions of README, app.js.

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # guessing-game
+The author of this program is Ron Dunphy. This is a demo for class 201d5. This is a guessing game created with HTML and JavaScript.  The user will be asked 3 yes or no questions about the creator of the game. Your correct answers will be tallied and displayed in a prompt box at the end of the game.  
--- app.js
+++ app.js
@@ -7,7 +7,7 @@ alert('Hi there, ' + user + ', I am going to ask you a few questions about mysel
 
 var answer1 = prompt(user +', Am I from Seattle? Please answer with "Y" or "N"');
 
-if (answer1 === 'Y' || answer1 === 'y' || answer1 === 'yes' || answer1 === 'YES') {
+if (answer1 === 'Y' || answer1 === 'y' || answer1 === 'yes' || answer1 === 'YES' || answer2 ===  'Yes') {
   alert('You are correct!');
   counter += 1
 } else {
@@ -15,9 +15,9 @@ if (answer1 === 'Y' || answer1 === 'y' || answer1 === 'yes' || answer1 === 'YES'
 
 }
 
-var answer2 = prompt(user + ', Was I born in Germany? Please answer with "Y" or "N"');
+var answer2 = prompt(user + ', Was I born in Germany? Please answer with "Yes" or "No"');
 
-if (answer2 === 'Y' || answer2 ===  'y' || answer2 ===  'yes' || answer2 ===  'YES') {
+if (answer2 === 'Y' || answer2 ===  'y' || answer2 ===  'yes' || answer2 ===  'YES' || answer2 ===  'Yes') {
   alert('You are correct, I was born in Wiesbaden Germany');
   counter =+ 1;
 } else {
@@ -26,7 +26,7 @@ if (answer2 === 'Y' || answer2 ===  'y' || answer2 ===  'yes' || answer2 ===  'Y
 
 var answer3 = prompt(user + ', Have I ever seen Star Wars? Please answer with "Y" or "N"');
 
-if (answer3 === 'Y' || answer3 ===  'y' || answer3 ===  'yes' || answer3 ===  'YES') {
+if (answer3 === 'Y' || answer3 ===  'y' || answer3 ===  'yes' || answer3 ===  'YES' || answer2 ===  'Yes') {
   alert('You are wrong, I am the last one.');
 } else {
   alert('That\'s right, I have not seen Star Wars.')
@@ -37,7 +37,6 @@ var number = 5;
 var answer4 = prompt('I\'m thinking of a number 1 through 10, see if you can guess which number it is.');
 
 if (answer4 == 5) {
-  alert('Good work!');
   counter += 1
 }
 while (answer4 != 5) {
@@ -49,3 +48,6 @@ if (answer4 <= 4) {
 var answer4 = prompt('I\'m thinking of a number 1 through 10, see if you can guess which number it is.');
 }
 alert('Good work!');
+
+console.log(counter);
+alert('You got ' + counter + ' questions correct, nice work ' + user + '!');
