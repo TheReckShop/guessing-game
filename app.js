265df622d726ad6d0c1b664d87b7cb61b5a81c25
YOUR FULL NAME <YOUR EMAIL ADDRESS>
2016-01-07 15:17:06 -0800

    made questions into functions to display code to html doc

--- app.js
+++ app.js
@@ -1,65 +1,80 @@
+var answer1 = document.getElementById('resultOne');
+var answer2 = document.getElementById('resultTwo');
+var answer3 = document.getElementById('resultThree');
+var answer4 = document.getElementById('resultFour');
+var answer5 = document.getElementById('resultFive');
+var total = document.getElementById('total');
 
-var counter = 0
 var user = prompt("What is your name?");
 console.log('The user\'s name is ' + user);
+var counter = 0;
 
-alert('Hi there, ' + user + ', I am going to ask you a few questions about myself.');
+function firstQuestion() {
+  var answer1 = prompt(user +', Am I from Seattle? Please answer with "Y" or "N"');
 
-var answer1 = prompt(user +', Am I from Seattle? Please answer with "Y" or "N"');
-
-if (answer1 === 'Y' || answer1 === 'y' || answer1 === 'yes' || answer1 === 'YES' || answer2 ===  'Yes') {
-  alert('You are correct!');
-  counter += 1
-} else {
-  alert('Sorry you are wrong, I am from Seattle.');
+  if (answer1.toUpperCase() === 'Y'|| answer1.toUpperCase() === 'YES') {
+    resultOne.textContent = 'You are correct!';
+    counter += 1;
+  } else {
+    resultOne.textContent = 'Sorry you are wrong, I am from Seattle.';
+  }
 }
 
+function secondQuestion() {
+  var answer2 = prompt(user + ', Was I born in Germany? Please answer with "Yes" or "No"');
 
-
-var answer2 = prompt(user + ', Was I born in Germany? Please answer with "Yes" or "No"');
-
-if (answer2 === 'Y' || answer2 ===  'y' || answer2 ===  'yes' || answer2 ===  'YES' || answer2 ===  'Yes') {
-  alert('You are correct, I was born in Wiesbaden Germany');
-  counter =+ 1;
-} else {
-  alert('I\'m sorry, that is incorrect.');
+  if (answer2.toUpperCase() === 'Y' || answer2.toUpperCase() ===  'YES') {
+    resultTwo.textContent = 'You are correct, I was born in Wiesbaden Germany';
+    counter =+ 1;
+  } else {
+    resultTwo.textContent = 'I\'m sorry, that is incorrect';
+  }
+}
+function thirdQuestion() {
+  var answer3 = prompt(user + ', Have I ever seen Star Wars? Please answer with "Y" or "N"');
+  if (answer3.toUpperCase() === 'Y' || answer3.toUpperCase() ===  'YES' ) {
+      resultThree.textContent = 'You are wrong, I am the last one that hasn\'t seen it.';
+  } else {
+      resultThree.textContent = 'That\'s right, I have not seen Star Wars.';
+      counter += 1;
+  }
 }
 
-var answer3 = prompt(user + ', Have I ever seen Star Wars? Please answer with "Y" or "N"');
+function fourthQuestion() {
+  var number = 5;
+  var answer4 = prompt('I\'m thinking of a number 1 through 10, see if you can guess which number it is.');
 
-if (answer3 === 'Y' || answer3 ===  'y' || answer3 ===  'yes' || answer3 ===  'YES' || answer2 ===  'Yes') {
-  alert('You are wrong, I am the last one.');
-} else {
-  alert('That\'s right, I have not seen Star Wars.')
-  counter += 1
+  if (answer4 == 5) {
+    counter += 1;
+  }
+  while (answer4 != 5) {
+    if (answer4 <= 4) {
+      resultFour.textContent = 'You are too low, guess higher.';
+    } else if (answer4 >= 6) {
+      resultFour.textContent = 'You are too high, guess lower.';
+    }
+    var answer4 = prompt('I\'m thinking of a number 1 through 10, see if you can guess which number it is.');
+  }
+    resultFour.textContent = 'Good work!';
 }
 
+function fifthQuestion() {
 
-var number = 5;
-var answer4 = prompt('I\'m thinking of a number 1 through 10, see if you can guess which number it is.');
+  var answer5 = prompt('Are The Meters the greatest band ever? Please answer "Y" or "N"');
 
-if (answer4 == 5) {
-  counter += 1
-}
-while (answer4 != 5) {
-if (answer4 <= 4) {
-  alert('You are too low, guess higher.');
-} else if (answer4 >= 6) {
-  alert('You are too high, guess lower.');
-}
-var answer4 = prompt('I\'m thinking of a number 1 through 10, see if you can guess which number it is.');
-}
-alert('Good work!');
-
-
-var answer5 = prompt('Are The Meters the greatest band ever? Please answer "Y" or "N"');
-
-if (answer5.toUpperCase() == "Y") {
-  alert(' That\'s right, ' + user + ' The Meters are the greatest band ever!')
-  counter += 1
-} else {
-  alert('I\'m sorry, but you\'re crazy!')
+  if (answer5.toUpperCase() == "Y" || answer5.toUpperCase() == 'YES') {
+    resultFive.textContent = 'That\'s right, ' + user + ' The Meters are the greatest band ever!';
+    counter += 1;
+  } else {
+    resultFive.textContent = 'I\'m sorry, but you\'re crazy!';
+  }
 }
 
+firstQuestion();
+secondQuestion();
+thirdQuestion();
+fourthQuestion();
+fifthQuestion();
+
+total.textContent = 'You got ' + counter + ' questions correct, nice work ' + user;
 console.log(counter);
-alert('You got ' + counter + ' questions correct, nice work ' + user + '!');
